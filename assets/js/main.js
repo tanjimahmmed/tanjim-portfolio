3700fdffc27a3bbfc9f8a7ac95a9acb9013c5f0a
tanjimahmmed <tanjimahmmed.8@gmail.com>
2020-07-18 13:01:48 +0600

    header padding decrement
    header padding decrement  and some portfolio added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,115 @@
-(function ($) {
-	"use strict";
+(function($) {
+    "use strict";
 
-    jQuery(document).ready(function($){
+    jQuery(document).ready(function($) {
 
-    // -- Java Script animation set --//
-    var animation = false,
-    animationstring = 'animation',
-    keyframeprefix = '',
-    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-    pfx = '',
-    elm = document.createElement('div');
-    if (elm.style.animationName !== undefined) {
-    animation = true;
-    }
-    if (animation === false) {
-        for (var i = 0; i < domPrefixes.length; i++) {
-            if (elm.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
-                pfx = domPrefixes[i];
-                animationstring = pfx + 'Animation';
-                keyframeprefix = '-' + pfx.toLowerCase() + '-';
-                animation = true;
-                break;
+        // -- Java Script animation set --//
+        var animation = false,
+            animationstring = 'animation',
+            keyframeprefix = '',
+            domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+            pfx = '',
+            elm = document.createElement('div');
+        if (elm.style.animationName !== undefined) {
+            animation = true;
+        }
+        if (animation === false) {
+            for (var i = 0; i < domPrefixes.length; i++) {
+                if (elm.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
+                    pfx = domPrefixes[i];
+                    animationstring = pfx + 'Animation';
+                    keyframeprefix = '-' + pfx.toLowerCase() + '-';
+                    animation = true;
+                    break;
+                }
             }
         }
-    }
-     // -- Typed  Java Script-- //
-     if(animation){
-         var text = $('.homepage_hero_area .typer_title'),
-         textOne = "i'm Front-End Developer",
-         textTwo = "i'm Freelancer",
-         textThree = "let's work together",
-         textFour = "i can create awesome stuff";
 
-         if (!!$.prototype.typer){
-             text.typer([textOne, textTwo, textThree, textFour]);
-         }
-     }
-        //Progress Bars function
-        $(document).ready(function() {
-        var show = true;
-        $(window).on("scroll", function() {
-            if (!show) return false;
-            var w_top = $(window).scrollTop();
-            var e_top = $("#about__content").offset().top - 50;
-            console.log(w_top + " " + e_top);
-            if (w_top >= e_top) {
-                $(".skillbar").each(function() {
-                    $(this).find(".skill_bar").animate({
-                        width: $(this).attr("data-percent")
-                    }, 2000)
-                });
-                // skills numbers
-                var time = 2;
-                $('#mySkills').each(function() {
-                    $('label').each(function() {
-                        var
-                            i = 1,
-                            num = $(this).data('num'),
-                            step = 1000 * time / num,
-                            that = $(this),
-                            int = setInterval(function() {
-                                if (i <= num) {
-                                    that.html(i);
-                                } else {
-                                    clearInterval(int);
-                                }
-                                i++;
-                            }, step);
-                    });
-                });
-                show = false;
+        // -- Typed  Java Script-- //
+
+        if (animation) {
+            var text = $('.homepage_hero_area .typer_title'),
+                textOne = "i'm Front-End Developer",
+                textTwo = "i'm Freelancer",
+                textThree = "let's work together",
+                textFour = "i can create awesome stuff";
+            if (!!$.prototype.typer) {
+                text.typer([textOne, textTwo, textThree, textFour]);
             }
-        });
+        }
+
+        // -- Progress Bars function -- //
+
+        $(document).ready(function() {
+            var show = true;
+            $(window).on("scroll", function() {
+                if (!show) return false;
+                var w_top = $(window).scrollTop();
+                var e_top = $("#about__content").offset().top - 50;
+                console.log(w_top + " " + e_top);
+                if (w_top >= e_top) {
+                    $(".skillbar").each(function() {
+                        $(this).find(".skill_bar").animate({
+                            width: $(this).attr("data-percent")
+                        }, 2000)
+                    });
+                    // skills numbers
+                    var time = 2;
+                    $('#mySkills').each(function() {
+                        $('label').each(function() {
+                            var
+                                i = 1,
+                                num = $(this).data('num'),
+                                step = 1000 * time / num,
+                                that = $(this),
+                                int = setInterval(function() {
+                                    if (i <= num) {
+                                        that.html(i);
+                                    } else {
+                                        clearInterval(int);
+                                    }
+                                    i++;
+                                }, step);
+                        });
+                    });
+                    show = false;
+                }
+            });
         });
 
+        // -- counter js -- //
 
-     // -- counter js -- //
-     $('.counter_number span').counterUp({
-         delay: 10,
-         time: 1000
-     });
-            
+        $('.counter_number span').counterUp({
+            delay: 10,
+            time: 1000
+        });
     });
-     // -- isotope -- //
-     $(".portfolio_filter li").on('click', function(){
-         $(".portfolio_filter li").removeClass("active");
-         $(this).addClass("active");
-         var selector = $(this).attr('data-filter');
 
-         $(".portfolio_list").isotope({
-             filter: selector,
-         })
-     });
-     $(".portfolio_list").isotope();
+    // -- isotope -- //
 
-     // -- magnific popup -- //
-     $('.popup_link').magnificPopup({
+    $(".portfolio_filter li").on('click', function() {
+        $(".portfolio_filter li").removeClass("active");
+        $(this).addClass("active");
+        var selector = $(this).attr('data-filter');
+        $(".portfolio_list").isotope({
+            filter: selector,
+        })
+    });
+    $(".portfolio_list").isotope();
+
+    // -- magnific popup -- //
+
+    $('.popup_link').magnificPopup({
         type: 'image',
         gallery: {
             enabled: true
         }
     });
-     // -- Testimonial area -- //
-     
-     $(".testimonial_carousel").owlCarousel({
-        items:1,
+
+    // -- Testimonial area -- //
+
+    $(".testimonial_carousel").owlCarousel({
+        items: 1,
         loop: true,
         nav: false,
         dots: true,
@@ -126,8 +131,9 @@
         }
     });
 
-     // -- smooth scroll -- //
-     $(".smooth_menu a").on('click', function(e) {
+    // -- smooth scroll -- //
+
+    $(".smooth_menu a").on('click', function(e) {
         var anchor = $(this);
         $("html, body").stop().animate({
             scrollTop: $(anchor.attr('href')).offset().top - 50
@@ -135,21 +141,26 @@
         e.preventDefault();
     });
 
-     // -- mobile menu -- //
-     $(".menu_triggers").on('click', function(){
-         $(".offcanvas_menu").addClass("active");
-         $(".offcanvas_menu_overlay").addClass("active");
-     });
-     $(".menu_close i.fa, .offcanvas_menu_overlay ").on('click', function(){
-         $(".offcanvas_menu").removeClass("active");
-         $(".offcanvas_menu_overlay").removeClass("active");
-     });
-     // -- sticky menu -- //
-     $(".header_area").sticky({
+    // -- mobile menu -- //
+
+    $(".menu_triggers").on('click', function() {
+        $(".offcanvas_menu").addClass("active");
+        $(".offcanvas_menu_overlay").addClass("active");
+    });
+    $(".menu_close i.fa, .offcanvas_menu_overlay ").on('click', function() {
+        $(".offcanvas_menu").removeClass("active");
+        $(".offcanvas_menu_overlay").removeClass("active");
+    });
+
+    // -- sticky menu -- //
+
+    $(".header_area").sticky({
         topSpacing: 0,
-     });
-     // -- popup js -- //
-    $('#inline-popups,#inline-popups-2,#inline-popups-3,#inline-popups-4,#inline-popups-5,#inline-popups-6,#inline-popups-7,#inline-popups-8,#inline-popups-9,#inline-popups-10,#inline-popups-11,#inline-popups-12,#inline-popups-13,#inline-popups-14,#inline-popups-15,#inline-popups-16').magnificPopup({
+    });
+
+    // -- popup js -- //
+
+    $('#inline-popups,#inline-popups-2,#inline-popups-3,#inline-popups-4,#inline-popups-5,#inline-popups-6,#inline-popups-7,#inline-popups-8,#inline-popups-9,#inline-popups-10,#inline-popups-11,#inline-popups-12,#inline-popups-13,#inline-popups-14,#inline-popups-15,#inline-popups-16,#inline-popups-17,#inline-popups-18,#inline-popups-19').magnificPopup({
         delegate: 'a',
         removalDelay: 400, //delay removal by X to allow out-animation
         callbacks: {
@@ -159,20 +170,18 @@
         },
         midClick: true
     });
-     // -- wow -- //
+
+    // -- wow -- //
+    
     var wow = new WOW({
-    mobile: false
+        mobile: false
     });
     wow.init();
-     
-
-
-   jQuery(window).load(function(){
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-   });
-
-}(jQuery));	
+    jQuery(window).load(function() {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
+        }
+    });
+}(jQuery));
